c1be10c98d87cf4b55486a39ab8105e3b1871d9d
Aku Sarma <akusarma1702@gmail.com>
2020-12-29 23:01:38 +0530

    fixing builder loadfile error

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ os.makedirs(f"{place}/{name}")
 
 try:
     with open(f"{place}/{name}/main.py", "w", encoding = 'utf-8') as p:
-        p.write(f"from kivy.app import App\nfrom kivy.lang import Builder\nfrom kivy.uix.widget import Widget\n\nBuilder.loadfile('{name}.kv')\n\nclass Main(Widget):\n\tpass\n\nclass MyApp(App):\n\tdef build(self):\n\t\treturn Main()\n\nMyApp().run()")
+        p.write(f"from kivy.app import App\nfrom kivy.lang import Builder\nfrom kivy.uix.widget import Widget\n\nBuilder.load_file('{name}.kv')\n\nclass Main(Widget):\n\tpass\n\nclass MyApp(App):\n\tdef build(self):\n\t\treturn Main()\n\nMyApp().run()")
 
     with open(f"{place}/{name}/{name}.kv", "w", encoding='utf-8') as k:
         k.write("#:kivy 2.0.0\n\n<Main>:")
